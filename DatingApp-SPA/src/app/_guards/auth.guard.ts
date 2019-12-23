f882291f0d21c9b9f78316b94c623334b9639ed2
unknown <Wilber.Palomino@LAPTOP-8C5HPQEL.saxony.local>
2019-12-23 14:56:03 -0600

    end of module 8

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   canActivate(): boolean {
-    if (this.authService.loggedIn()){
+    if (this.authService.loggedIn()) {
       return true;
     }
 
